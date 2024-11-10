939e763af515f149573666e69048ff5579ad83be
Gustavo Henrique <gustavohenrique@MacBook-Air-de-Gustavo.local>
2024-11-10 19:05:38 -0300

    Commit inicial - configuração do projeto

---
+++ src/main/java/com/vrbeneficios/miniautorizador/MiniAutorizadorVrBeneficiosApplication.java
@@ -0,0 +1,13 @@
+package com.vrbeneficios.miniautorizador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniAutorizadorVrBeneficiosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniAutorizadorVrBeneficiosApplication.class, args);
+	}
+
+}
