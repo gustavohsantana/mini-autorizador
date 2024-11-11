869248d6115da5b0be578d9fc90a5dadb839e3a2
gustavohenrique <gustavosouza@gec.inatel.br>
2024-11-10 21:39:46 -0300

    Properties Configs e Unit Test para persistencia de Dados

--- src/main/java/com/vrbeneficios/miniautorizador/model/Cartao.java
+++ src/main/java/com/vrbeneficios/miniautorizador/model/Cartao.java
@@ -2,10 +2,12 @@ package com.vrbeneficios.miniautorizador.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Entity
 @Data
+@AllArgsConstructor
 public class Cartao {
 
     @Id
---
+++ src/test/java/com/vrbeneficios/miniautorizador/service/CartaoServiceTest.java
@@ -0,0 +1,25 @@
+package com.vrbeneficios.miniautorizador.service;
+
+import com.vrbeneficios.miniautorizador.model.Cartao;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+public class CartaoServiceTest {
+
+    @Autowired
+    private CartaoService cartaoService;
+
+    @Test
+    public void testCriarCartao() {
+        Cartao cartao = new Cartao("123456789012", "senha123", 1000.0);
+        Cartao novoCartao = cartaoService.criarCartao(cartao);
+        assertNotNull(novoCartao);
+        assertEquals(cartao.getNumeroCartao(), novoCartao.getNumeroCartao());
+    }
+
+}
